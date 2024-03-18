82c2a25dc1d8564c9043b7dccc7e76c3e8722c02
amgv13 <agambin1@alu.ucam.edu>
2024-03-18 09:51:46 +0100

    Creación de una clase Empleados donde hemos puesto el método aumentarSalario y listarEmpleados. Creación de un método en Empleados llamado DarDeAlta que añade los empleados creados en el main de SistemaGestionEmpleados a el array empleados(ahora llamado lista) que estaba en SistemaGestionEmpleados

---
+++ Empleados.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+class Empleados{
+
+    private ArrayList<Empleado> lista = new ArrayList<>();
+
+    public void listarEmpleados(){
+
+        System.out.println("Lista de Empleados:");
+
+        for (Empleado empleado : this.lista) {
+            System.out.println(empleado);
+        }
+
+    }
+
+    public void aumentarSalario(double porcentaje) {
+
+        for (Empleado empleado : this.lista) {
+            double nuevoSalario = empleado.getSalario() * (1 + porcentaje / 100);
+            empleado.setSalario(nuevoSalario);
+        }
+    }
+
+    public void darDeAlta(ArrayList<Empleado> empleados){
+        this.lista.addAll(empleados);
+    }
+    
+}
+
--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -1,27 +1,18 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SistemaGestionEmpleados {
 
-    private Empleado[] empleados;
-
-    public SistemaGestionEmpleados(Empleado[] empleados) {
-        this.empleados = empleados;
-    }
-
-    public void aumentarSalario(double porcentaje) {
-        for (Empleado empleado : empleados) {
-            double nuevoSalario = empleado.getSalario() * (1 + porcentaje / 100);
-            empleado.setSalario(nuevoSalario);
-        }
-    }
-
     public static void main(String[] args) {
-        Empleado[] empleados = new Empleado[3];
-        empleados[0] = new Empleado("Juan", "Desarrollador", 50000);
-        empleados[1] = new Empleado("María", "Diseñadora", 45000);
-        empleados[2] = new Empleado("Pedro", "Gerente", 60000);
 
-        SistemaGestionEmpleados sistema = new SistemaGestionEmpleados(empleados);
+        ArrayList<Empleado> empleados = new ArrayList<>();
+
+        empleados.add(new Empleado("Juan", "Desarrollador", 50000));
+        empleados.add(new Empleado("María", "Diseñadora", 45000));
+        empleados.add(new Empleado("Pedro", "Gerente", 60000));
+
+        Empleados sistema = new Empleados();
+        sistema.darDeAlta(empleados);
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Introduzca el porcentaje de aumento de salario: ");
@@ -29,10 +20,7 @@ public class SistemaGestionEmpleados {
 
         sistema.aumentarSalario(porcentaje);
         
-        System.out.println("Lista de Empleados:");
-        for (Empleado empleado : empleados) {
-            System.out.println(empleado);
-        }
+        sistema.listarEmpleados();
 
         scanner.close();
     }
